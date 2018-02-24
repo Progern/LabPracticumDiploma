c6e370bcfa9303aa1a7fa310fd76f0ac0cf3de81
Oleg <olegmiskoo@gmail.com>
2018-02-24 19:40:25 +0200

    Added root window with text label.

--- MainWindow.py
+++ MainWindow.py
@@ -1,3 +1,8 @@
-from tkinter import * # Import all packages from tkinter
+from tkinter import *  # Import all packages from tkinter
 
-rootWindow = Tk() # Creates a blank window
+rootWindow = Tk()  # Creates a blank window
+
+titleLabel = Label(rootWindow, text="Lab practicum application.")  # Creates a label with specified text
+titleLabel.pack()  # Set the label's initial position at first place, when it can be set
+
+rootWindow.mainloop()  # Infinite loop for UI
